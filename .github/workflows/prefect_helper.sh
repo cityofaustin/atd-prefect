2fc0b593a114c7e21ab7cca2f1c05107fa910643
Sergio Garcia <sergiogcx@gmail.com>
2021-12-06 16:45:00 -0600

    Add get_md5_from_cloud

--- .github/workflows/prefect_helper.sh
+++ .github/workflows/prefect_helper.sh
@@ -37,6 +37,17 @@ function get_md5_from_file() {
   md5sum $1 | cut -d " " -f 1;
 }
 
+# Downloads the md5 checksum from S3
+function get_md5_from_cloud() {
+  FILE_NAME_HASH=$(get_md5_for_filename $1);
+  {
+    aws s3 cp "s3://${BUCKET_NAME}/deploys/${WORKING_STAGE}/${FILE_NAME_HASH}.md5sum" "${FILE_NAME_HASH}.md5sum" --quiet;
+    cat "${FILE_NAME_HASH}.md5sum";
+  } || {
+    exit_with_error "Error: Failed to download md5 sum for '${1}'";
+  }
+}
+
 
 # Registers the tasks
 function register_tasks() {
