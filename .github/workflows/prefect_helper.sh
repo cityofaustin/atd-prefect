c65f2db54acfe12cef9d272811b95c64e333ce84
Sergio Garcia <sergiogcx@gmail.com>
2021-12-06 15:24:39 -0600

    Update bash script

--- .github/workflows/prefect_helper.sh
+++ .github/workflows/prefect_helper.sh
@@ -6,12 +6,12 @@ function register_tasks() {
   # First make sure to log in...
   prefect auth login --key "${PREFECT_KEY}";
 
-  # We need to retrieve the name of the project
-  for FLOW_PROJECT in $(ls "../../projects/"); do
-    # Then each individual file within the project
-    for FLOW_FILE in $(find "../../projects/${FLOW_PROJECT}" -type f -name "*.py"); do
-      echo "... ${FLOW_FILE}";
-      prefect register --project "${FLOW_PROJECT}" --path "${FLOW_FILE}"
-    done;
+  # For each of the flow files
+  for FLOW_FILE in $(grep -rl "__main__" flows); do
+    # Extract the project name
+    FLOW_PROJECT=$(echo "${FLOW_FILE}" | cut -d "/" -f 2);
+    echo "Processing flow file: ${FLOW_FILE} (project name: ${FLOW_PROJECT})";
+    # Register the flow file using the folder name as the project name
+    prefect register --project $FLOW_PROJECT --path $FLOW_FILE;
   done;
 }
