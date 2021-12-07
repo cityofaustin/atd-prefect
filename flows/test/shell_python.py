e89b7dbfb5e1758afe92b881159160fd1abc5a73
Sergio Garcia <sergiogcx@gmail.com>
2021-12-07 17:25:03 -0600

    Run shell and python example

---
+++ flows/test/shell_python.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+"""
+Name: Shell and python tests
+Description: This is a test on how to run shell and python commands
+Schedule: None
+Labels: test
+"""
+import docker
+from prefect import Flow, task
+from prefect.engine.state import Failed
+from prefect.run_configs import LocalRun
+
+# Slack
+from prefect.utilities.notifications import slack_notifier
+
+# Shell
+from prefect.tasks.shell import ShellTask
+
+# We can call it early
+handler = slack_notifier(only_states=[Failed])
+
+environment_variables = {
+    "MESSAGE": "HELLO WORLD"
+}
+
+
+shell_task = ShellTask(
+    name="shell_task",
+    command='echo "MESSAGE: ${MESSAGE}"',
+    env=environment_variables,
+    stream_output=True
+)
+
+python_task = ShellTask(
+    name="python_task",
+    command='python3 ./flows/test/scripts/example.py',
+    env=environment_variables,
+    stream_output=True
+)
+
+
+# Create the flow
+with Flow(
+    "shell-python-test",
+    run_config=LocalRun(labels=["test"])
+) as flow:
+    # Chain the two tasks
+    flow.chain(shell_task, python_task)
+
+if __name__ == "__main__":
+    flow.run()
