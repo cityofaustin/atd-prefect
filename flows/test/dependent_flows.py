b12fc0c750f86a75e047cb03c1a0ec6e639e9952
Chia Berry <analuciaby@gmail.com>
2022-02-23 13:29:33 -0600

    use add edge

--- flows/test/dependent_flows.py
+++ flows/test/dependent_flows.py
@@ -73,6 +73,7 @@ with Flow(
     result=PrefectResult()
 ) as first_flow:
     email_data = first()
+    print(first_flow.serialize()['tasks'])
 
 
 with Flow(
@@ -91,10 +92,11 @@ with Flow(
     ),
 ) as second_flow:
     first_flow_run_id = create_flow_run(flow_name=first_flow.name)
-    first_data = get_task_run_result(first_flow_run_id, task_slug="first-slug")
+    #first_data = get_task_run_result(first_flow_run_id, task_slug="first-slug-copy")
     # email_message = Parameter("email_message", default="there was nothing")
-    transform_and_show(first_data)
-    email_task(task_args=dict(msg=str(first_data)))
+    # transform_and_show(first_data)
+    #email_task(task_args=dict(msg=str(first_data)))
+    second_flow.add_edge(get_task_run_result(first_flow_run_id, task_slug="first-slug-copy"), email_task(task_args=dict(msg=str(first_data))))
 
 
 if __name__ == "__main__":
