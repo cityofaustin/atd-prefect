f46977414454cccb4bbaa27307cdf445a63146db
Frank Hereford <frank@frankhereford.com>
2022-04-27 14:41:55 -0500

    rename temp dir since we'll have two and form unzip command

--- flows/vision_zero/cris_import.py
+++ flows/vision_zero/cris_import.py
@@ -6,18 +6,20 @@ import time
 
 SFTP_ENDPOINT = os.getenv('SFTP_ENDPOINT')
 
-with tempfile.TemporaryDirectory() as tmpdir:
+with tempfile.TemporaryDirectory() as zip_tmpdir:
   sysrsync.run(
     verbose=True,
     options=['-a'],
     source_ssh=SFTP_ENDPOINT,
     source='/home/txdot/*zip',
     sync_source_contents=False,
-    destination=tmpdir
+    destination=zip_tmpdir
     )
-  print("Temp Directory:", tmpdir)
-  for filename in os.listdir(tmpdir):
+  print("Temp Directory:", zip_tmpdir)
+  for filename in os.listdir(zip_tmpdir):
     print("File:", filename)
+    with tempfile.TemporaryDirectory() as extract_tmpdir:
+    unzip_command = f'7za -y -p{ZIP_PASSWORD} -o"{extract_tmpdir}" x "{zip_tmpdir}"
 
 
 
