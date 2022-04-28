30cbd5817fb6f19b17f846656b469fadc29801d2
Frank Hereford <frank@frankhereford.com>
2022-04-28 10:20:05 -0500

    unzip each zip into its own directory

--- flows/vision_zero/cris_import.py
+++ flows/vision_zero/cris_import.py
@@ -18,5 +18,12 @@ with tempfile.TemporaryDirectory() as zip_tmpdir:
     )
   print("Temp Directory:", zip_tmpdir)
 
-  with tempfile.TemporaryDirectory() as extract_tmpdir:
-  unzip_command = f'7za -y -p{ZIP_PASSWORD} -o"{extract_tmpdir}" x "{zip_tmpdir}"'
+  for filename in os.listdir(zip_tmpdir):
+    print("File:", filename)
+
+    with tempfile.TemporaryDirectory() as extract_tmpdir:
+      unzip_command = f'7za -y -p{ZIP_PASSWORD} -o"{extract_tmpdir}" x "{zip_tmpdir}/{filename}"'
+      print("Unzip command:", unzip_command)
+      unzip_result = os.system(unzip_command)
+      time.sleep(60)
+
