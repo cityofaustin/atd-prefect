ea49ae672e4a63e4fecd32cdf0234ee750655058
Frank Hereford <frank@frankhereford.com>
2022-05-13 08:55:27 -0500

    don't need container ID here

--- flows/vision-zero/prefect_bootstrap/start_agent.sh
+++ flows/vision-zero/prefect_bootstrap/start_agent.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 prefect auth login -k $VZ_PREFECT_API_KEY
-#prefect agent local start -l vision-zero -l atd-data03
+#prefect agent local start --no-hostname-label -l vision-zero -l atd-data03
 tail -f /dev/null
