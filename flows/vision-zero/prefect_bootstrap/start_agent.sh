034c6450b4303af59101f64efb9d38ce4181f5a0
Frank Hereford <frank@frankhereford.com>
2022-05-13 08:53:45 -0500

    image entrypoint

--- flows/vision-zero/prefect_bootstrap/start_agent.sh
+++ flows/vision-zero/prefect_bootstrap/start_agent.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 prefect auth login -k $VZ_PREFECT_API_KEY
-prefect agent local start -l vision-zero -l atd-data03
-#tail -f /dev/null
+#prefect agent local start -l vision-zero -l atd-data03
+tail -f /dev/null
