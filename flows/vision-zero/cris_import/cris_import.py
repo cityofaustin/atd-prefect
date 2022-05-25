f2370de703e8b04e4affe41af06bb92cb13076bd
Frank Hereford <frank@frankhereford.com>
2022-05-25 09:44:35 -0500

    Switch to KV store

--- flows/vision-zero/cris_import/cris_import.py
+++ flows/vision-zero/cris_import/cris_import.py
@@ -26,20 +26,22 @@ from prefect.backend import get_key_value
 from prefect.run_configs import UniversalRun
 
 kv_store = get_key_value('Vision Zero Development')
+kv_dictionary = json.loads(kv_store)
 
-PWD = os.getenv("PWD")
-SFTP_ENDPOINT = os.getenv("SFTP_ENDPOINT")
-ZIP_PASSWORD = os.getenv("ZIP_PASSWORD")
-RAW_AIRFLOW_CONFIG_JSON = os.getenv("RAW_AIRFLOW_CONFIG")
-VZ_ETL_LOCATION = os.getenv("VZ_ETL_LOCATION")
+
+SFTP_ENDPOINT = kv_dictionary["SFTP_ENDPOINT"]
+ZIP_PASSWORD = kv_dictionary["ZIP_PASSWORD"]
+VZ_ETL_LOCATION = kv_dictionary["VZ_ETL_LOCATION"]
+
+RAW_AIRFLOW_CONFIG_JSON = kv_dictionary["RAW_AIRFLOW_CONFIG"]
 RAW_AIRFLOW_CONFIG = json.loads(RAW_AIRFLOW_CONFIG_JSON)
 
-AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_CSV_ARCHIVE_BUCKET_NAME = os.getenv("AWS_CSV_ARCHIVE_BUCKET_NAME")
-AWS_CSV_ARCHIVE_PATH_PRODUCTION = os.getenv("AWS_CSV_ARCHIVE_PATH_PRODUCTION")
-AWS_CSV_ARCHIVE_PATH_STAGING = os.getenv("AWS_CSV_ARCHIVE_PATH_STAGING")
+AWS_DEFAULT_REGION = kv_dictionary["AWS_DEFAULT_REGION"]
+AWS_ACCESS_KEY_ID = kv_dictionary["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = kv_dictionary["AWS_SECRET_ACCESS_KEY"]
+AWS_CSV_ARCHIVE_BUCKET_NAME = kv_dictionary["AWS_CSV_ARCHIVE_BUCKET_NAME"]
+AWS_CSV_ARCHIVE_PATH_PRODUCTION = kv_dictionary["AWS_CSV_ARCHIVE_PATH_PRODUCTION"]
+AWS_CSV_ARCHIVE_PATH_STAGING = kv_dictionary["AWS_CSV_ARCHIVE_PATH_STAGING"]
 
 
 def skip_if_running_handler(obj, old_state, new_state):
