cba122a34f68e1bf55503895dcd1f88c9f84d01b
Frank Hereford <frankhereford@users.noreply.github.com>
2022-07-24 09:06:08 -0500

    Change dev command for the time being

--- flows/moped/test_instance_deployment/dev_command.sh
+++ flows/moped/test_instance_deployment/dev_command.sh
@@ -7,5 +7,5 @@ black ./test_instance_deployment/atd-moped/moped-etl/prefect/tasks/ecs.py;
 black ./test_instance_deployment/atd-moped/moped-etl/prefect/tasks/netlify.py;
 black ./test_instance_deployment/atd-moped/moped-etl/prefect/tasks/api.py;
 black ./test_instance_deployment/atd-moped/moped-etl/prefect/tasks/database.py;
-python3 ./test_instance_deployment/atd-moped/moped-etl/prefect/moped_test_create_flow.py | tee output_log; 
-vim output_log;
+#python3 ./test_instance_deployment/atd-moped/moped-etl/prefect/moped_test_create_flow.py | tee output_log; vim output_log;
+python3 ./test_instance_deployment/atd-moped/moped-etl/prefect/moped_test_create_flow.py 
