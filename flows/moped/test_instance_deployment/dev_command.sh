e4235ca5699c0f679f13b5ab84b559ab9aa40de0
Frank Hereford <frankhereford@users.noreply.github.com>
2022-07-24 08:49:24 -0500

    Lint on execution for more files

--- flows/moped/test_instance_deployment/dev_command.sh
+++ flows/moped/test_instance_deployment/dev_command.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-
 clear;
 rm output_log;
-black ./test_instance_deployment/atd-moped/moped-etl/prefect/moped_test_create_flow.py ./test_instance_deployment/atd-moped/moped-etl/prefect/tasks/ecs.py;
+black ./test_instance_deployment/atd-moped/moped-etl/prefect/moped_test_create_flow.py 
+black ./test_instance_deployment/atd-moped/moped-etl/prefect/tasks/ecs.py;
+black ./test_instance_deployment/atd-moped/moped-etl/prefect/tasks/netlify.py;
+black ./test_instance_deployment/atd-moped/moped-etl/prefect/tasks/api.py;
+black ./test_instance_deployment/atd-moped/moped-etl/prefect/tasks/database.py;
 python3 ./test_instance_deployment/atd-moped/moped-etl/prefect/moped_test_create_flow.py | tee output_log; 
 vim output_log;
