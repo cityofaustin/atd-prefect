6ea514e252d6f806ffc686b4e9d1c9eb379885fb
Charlie Henry <30810522+Charlie-Henry@users.noreply.github.com>
2022-07-27 16:21:59 -0500

    cleaning things up

--- flows/microstrategy/mstro_to_s3.py
+++ flows/microstrategy/mstro_to_s3.py
@@ -1,55 +1,59 @@
 # Standard Library imports
+import datetime
 from io import StringIO
 import os
 
-# Related third party imports
-import pandas as pd
-import boto3
+# Prefect imports
 from prefect import task, Flow, Parameter, unmapped
+from prefect.engine.state import Failed
 from prefect.schedules import IntervalSchedule
 from prefect.run_configs import UniversalRun, DockerRun
 from prefect.backend import set_key_value, get_key_value
 from prefect.storage import Docker, GitHub
+from prefect.utilities.notifications import slack_notifier
+
+
+# Related third party imports
+import boto3
 from mstrio.connection import Connection
 from mstrio.project_objects import Report
+import pandas as pd
 
+handler = slack_notifier(only_states=[Failed])
+
+environment_variables = get_key_value(key=f"atd_microstrategy")
 
 ENV = "test"
 
 # Microstrategy Credentials
 PROJECT_ID = "6B64D80C11E1AFEA001000805B2705A0"
 BASE_URL = "https://mstrprod-library.austintexas.gov/MicroStrategyLibrary/api/"
-
-environment_variables = get_key_value(key=f"atd_microstrategy")
-
 MSTRO_USERNAME = environment_variables["MSTRO_USERNAME"]
 MSTRO_PASSWORD = environment_variables["MSTRO_PASSWORD"]
 
-# 2D array where each row is a report to be retrived
+# 2D list of default reports to be retrived
 ## [File Name for S3, Report ID]
 # To find report ID, go to the report in Microstrategy then:
 ## Go to Tools > Report Details Page or Document Details Page.
 ## Click Show Advanced Details button at the bottom
-# REPORTS = [
-#     ["2020 Bond Expenses Obligated", "85A9E0A049F06D98AF1CF3BE8CDA9394"],
-#     ["All Bonds Expenses Obligated", "aaabb"],
-# ]
-
-REPORTS = [
-    ["2020 Bond Expenses Obligated", "85A9E0A049F06D98AF1CF3BE8CDA9394"],
-    # ["All Bonds Expenses Obligated", "aaabb"],
-]
-
-report_names = [i[0] for i in REPORTS]
-report_ids = [i[1] for i in REPORTS]
+# Report name should be unique as it is the file name in the S3 bucket
+REPORTS = [["2020 Bond Expenses Obligated", "85A9E0A049F06D98AF1CF3BE8CDA9394"]]
 
 ## AWS Credentials
 AWS_ACCESS_ID = environment_variables["AWS_ACCESS_ID"]
 AWS_PASS = environment_variables["AWS_PASS"]
 BUCKET_NAME = environment_variables["BUCKET"]
 
+# Report names and ids separated from 2D list
+report_names = [i[0] for i in REPORTS]
+report_ids = [i[1] for i in REPORTS]
 
-@task
+# Returns a connection object for interacting with the microstrategy API
+@task(
+    max_retries=2,
+    retry_delay=datetime.timedelta(minutes=10),
+    # state_handlers=[handler],
+)
 def connect_to_mstro():
     conn = Connection(
         base_url=BASE_URL,
@@ -61,7 +65,12 @@ def connect_to_mstro():
     return conn
 
 
-@task
+# Returns a s3 object for interacting with the boto3 AWS S3 API
+@task(
+    max_retries=2,
+    retry_delay=datetime.timedelta(minutes=10),
+    # state_handlers=[handler],
+)
 def connect_to_AWS():
     session = boto3.Session(
         aws_access_key_id=AWS_ACCESS_ID, aws_secret_access_key=AWS_PASS,
@@ -71,14 +80,26 @@ def connect_to_AWS():
     return s3_res
 
 
-@task()
+# Downloads a report from microstrategy with a given report_id
+# returns it as a pandas dataframe
+@task(
+    max_retries=2,
+    retry_delay=datetime.timedelta(minutes=10),
+    # state_handlers=[handler],
+)
 def download_report(report_id, conn):
     my_report = Report(connection=conn, id=report_id, parallel=False)
-    my_report_df = my_report.to_dataframe()
-    return my_report_df
+    return my_report.to_dataframe()
 
 
-@task
+# Takes a pandas dataframe and formats it to be sent as a .csv in an S3 bucket
+# Uses the report_name.csv as a file name
+# report_name should be unique or it'll overwrite another report
+@task(
+    max_retries=2,
+    retry_delay=datetime.timedelta(minutes=10),
+    # state_handlers=[handler],
+)
 def report_to_s3(df, report_name, s3):
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index=False)
@@ -86,38 +107,38 @@ def report_to_s3(df, report_name, s3):
     s3.Object(BUCKET_NAME, file_name).put(Body=csv_buffer.getvalue())
 
 
+# Define prefect flow
 with Flow(
     f"microstrategy_to_s3_{ENV}",
-    schedule=None,
-    #run_config=UniversalRun(labels=[ENV, "Charlies-MBP.attlocal.net"]),
-    run_config=DockerRun(labels=[ENV, "charliemacbook"]),
-    #storage=GitHub(
-    #     repo="cityofaustin/atd-prefect",
-    #     path="flows/microstrategy/mstro_to_s3.py",
-    #     ref="microstrategy-reports",  # The branch name
-    #),
+    schedule=None,  # initalizing with no schedule
+    # DockerRun needs a docker agent
+    run_config=DockerRun(labels=[ENV, "docker", "atd-data03"]),
 ) as flow:
+    # Mapping parameters
     ids = Parameter("report_ids", default=report_ids, required=True)
-
     names = Parameter("report_names", default=report_names, required=True)
     flow.add_task(names)
 
+    # 1. Get microstrategy connection
     conn = connect_to_mstro()
+
+    # 2. Conenct to S3
     s3 = connect_to_AWS()
+
+    # 3. Download report to df (for each report_id)
     df = download_report.map(ids, unmapped(conn))
+
+    # 4. Send file to S3 bucket (for each report_id)
     report_to_s3.map(df, names, unmapped(s3))
 
-#flow.storage = Docker(
-#   #registry_url="https://registry.hub.docker.com",
-#   #image_name="atddocker/atd-microstrategy",
-#    #image_tag="test",
-#    dockerfile="Dockerfile"
-#)
-
 flow.storage = Docker(
-    python_dependencies= ["mstrio-py","pandas","boto3"],
-    #image_name="atddocker/atd-microstrategy",
-    #image_tag="test",
+    registry_url="",  # Needs to be empty string for pushing to dockerhub
+    image_name="atddocker/atd-microstrategy",
+    image_tag=ENV,
+    python_dependencies=["mstrio-py", "pandas", "boto3"],
+    # Can also/either include dockerfile = "path/to/Dockerfile" if you need to
+    # better define your docker image.
 )
 
+# Defining default parameters for flow run
 flow.run(parameters={"report_ids": report_ids, "report_names": report_names})
