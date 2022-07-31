5f4411228334d52da6fcc736252ef89b82a69468
Frank Hereford <frankhereford@users.noreply.github.com>
2022-07-31 14:13:04 -0500

    the other tables

--- flows/vision-zero/cris_import/lib/mappings.py
+++ flows/vision-zero/cris_import/lib/mappings.py
@@ -2,4 +2,8 @@
 def get_table_map():
     return {
         'crash': 'atd_txdot_crashes',
+        'unit': 'atd_txdot_units',
+        'person': 'atd_txdot_person',
+        'primaryperson': 'atd_txdot_primaryperson',
+        'charges': 'atd_txdot_charges',
         }
