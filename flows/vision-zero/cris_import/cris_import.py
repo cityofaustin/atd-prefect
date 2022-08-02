522eef2b2ad5fd6e54c624c4049ce9168d8fdbeb
Frank Hereford <frankhereford@users.noreply.github.com>
2022-08-01 20:46:52 -0500

    When updating, compute list of changed columns - useful for debugging

--- flows/vision-zero/cris_import/cris_import.py
+++ flows/vision-zero/cris_import/cris_import.py
@@ -544,6 +544,7 @@ def align_records(typed_token):
 
                 column_assignments = []
                 column_comparisons = []
+                column_aggregators = []
                 for column in target_columns:
                     if (not column["column_name"] in no_override_columns) and column[
                         "column_name"
@@ -552,7 +553,7 @@ def align_records(typed_token):
                             f"{column['column_name']} = {DB_IMPORT_SCHEMA}.{table}.{column['column_name']}"
                         )
                         column_comparisons.append(
-                            # public.atd_txdot_crashes.crash_fatal_fl = import.crash.crash_fatal_fl
+                            # there are two ways to be equal. Either be of the same value and type or /both/ be undefined
                             f"""
                             (
                                 public.{output_map[table]}.{column['column_name']} = {DB_IMPORT_SCHEMA}.{table}.{column['column_name']}
@@ -560,6 +561,15 @@ def align_records(typed_token):
                                 ( public.{output_map[table]}.{column['column_name']} IS NULL AND {DB_IMPORT_SCHEMA}.{table}.{column['column_name']} IS NULL )
                             )
                             """
+                        )
+                        column_aggregators.append(
+                            f"""
+                            case when not (
+                                public.{output_map[table]}.{column['column_name']} = {DB_IMPORT_SCHEMA}.{table}.{column['column_name']}
+                                or
+                                (public.{output_map[table]}.{column['column_name']} is null and {DB_IMPORT_SCHEMA}.{table}.{column['column_name']} is null)
+                            ) then '{column['column_name']}' else null end
+                        """
                         )
 
                 sql = (
@@ -573,16 +583,31 @@ def align_records(typed_token):
                 )
                 sql += f"where {public_key_sql}\n"
 
-                # print(sql)
-
                 cursor = pg.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
                 cursor.execute(sql)
                 skip_update_query = cursor.fetchone()
-                # print(skip_update_query)
-                # time.sleep(60)
                 if skip_update_query["skip_update"]:
                     print(f"Skipping update for {output_map[table]} {public_key_sql}")
                     continue
+
+                sql = "select "
+                sql += (
+                    "array_remove(array["
+                    + ",".join(column_aggregators)
+                    + "], null)"
+                    + "as changed_columns "
+                )
+                sql += f"from public.{output_map[table]} "
+                sql += (
+                    f"left join {DB_IMPORT_SCHEMA}.{table} on ("
+                    + " and ".join(linkage_clauses)
+                    + ")\n"
+                )
+                sql += f"where {public_key_sql}\n"
+                cursor = pg.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+                cursor.execute(sql)
+                changed_columns = cursor.fetchone()
+                print("Changed Columns: " + str(changed_columns["changed_columns"]))
 
                 sql = "update public." + output_map[table] + " set "
                 # this next line adds the column assignments generated above into this query.
