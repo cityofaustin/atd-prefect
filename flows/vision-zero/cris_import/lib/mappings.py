0ca4e8e4e1d64c5b95d22c69d19fef9b02cf7f76
Frank Hereford <frankhereford@users.noreply.github.com>
2022-08-01 22:01:22 -0500

    disable a table in a more thoughtful way

--- flows/vision-zero/cris_import/lib/mappings.py
+++ flows/vision-zero/cris_import/lib/mappings.py
@@ -4,7 +4,8 @@ def get_table_map():
         "unit": "atd_txdot_units",
         "person": "atd_txdot_person",
         "primaryperson": "atd_txdot_primaryperson",
-        "charges": "atd_txdot_charges",
+        # this table needs to be reworked to use this system
+        # "charges": "atd_txdot_charges",
     }
 
 
