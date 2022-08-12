4658181c5d3ddbab15b73667cebc9c069e3c4f6e
Frank Hereford <frankhereford@users.noreply.github.com>
2022-08-12 11:59:13 -0500

    tell agent where it can find the task libs

--- flows/moped/prefect_bootstrap/start_agent.sh
+++ flows/moped/prefect_bootstrap/start_agent.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 prefect auth login -k $PREFECT_API_KEY
-prefect agent local start -l moped 
+prefect agent local start -l moped --import-path /root/test_instance_deployment/atd-moped/moped-etl/prefect
