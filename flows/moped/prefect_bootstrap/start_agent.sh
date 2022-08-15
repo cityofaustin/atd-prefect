bbd2c34eea5d102b47e5581bf104d01b257d6b92
Frank Hereford <frankhereford@users.noreply.github.com>
2022-08-15 16:50:17 -0500

    two dask workers is enough for this flow

--- flows/moped/prefect_bootstrap/start_agent.sh
+++ flows/moped/prefect_bootstrap/start_agent.sh
@@ -3,7 +3,6 @@
 prefect auth login -k $PREFECT_API_KEY
 
 dask-scheduler --dashboard-address :8787 &
-#dask-worker tcp://prefect-agent:8786 &
 dask-worker tcp://prefect-agent:8786 &
 dask-worker tcp://prefect-agent:8786 &
 
