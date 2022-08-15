827b6eb8519af0854c3b767a1466809ef9095a9a
Frank Hereford <frankhereford@users.noreply.github.com>
2022-08-15 01:20:10 -0500

    spin up a dask monitor and three workers

--- flows/moped/prefect_bootstrap/start_agent.sh
+++ flows/moped/prefect_bootstrap/start_agent.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 prefect auth login -k $PREFECT_API_KEY
+
+dask-scheduler &
+dask-worker tcp://172.25.0.2:8786 &
+dask-worker tcp://172.25.0.2:8786 &
+dask-worker tcp://172.25.0.2:8786 &
+
 prefect agent local start -l moped --import-path /root/test_instance_deployment/atd-moped/moped-etl/prefect
