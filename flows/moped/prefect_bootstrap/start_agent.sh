9f64d336bb28a6c3d1eb28a0b15f1c6943cf49c7
Frank Hereford <frankhereford@users.noreply.github.com>
2022-08-15 01:28:47 -0500

    thank you docker-compose

--- flows/moped/prefect_bootstrap/start_agent.sh
+++ flows/moped/prefect_bootstrap/start_agent.sh
@@ -3,8 +3,8 @@
 prefect auth login -k $PREFECT_API_KEY
 
 dask-scheduler &
-dask-worker tcp://172.25.0.2:8786 &
-dask-worker tcp://172.25.0.2:8786 &
-dask-worker tcp://172.25.0.2:8786 &
+dask-worker tcp://prefect-agent:8786 &
+dask-worker tcp://prefect-agent:8786 &
+dask-worker tcp://prefect-agent:8786 &
 
 prefect agent local start -l moped --import-path /root/test_instance_deployment/atd-moped/moped-etl/prefect
