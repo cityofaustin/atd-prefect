3bea956e002c2e04041a21f7fb5c5cf0545cc0f9
Frank Hereford <frankhereford@users.noreply.github.com>
2022-08-25 18:13:20 -0500

    add path for import

--- flows/vision-zero/prefect_bootstrap/start_agent.sh
+++ flows/vision-zero/prefect_bootstrap/start_agent.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 prefect auth login -k $VZ_PREFECT_API_KEY
-prefect agent local start -l vision-zero --import-path /root/cris_import
+prefect agent local start -l vision-zero --import-path /root/cris_import --import-path /root/cris_import/atd-vz-data/atd-etl/app
