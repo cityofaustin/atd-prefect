1e6c7e79ea3905643a6316b04b8238fbb6b0d26d
Rose <67130963+roseeichelmann@users.noreply.github.com>
2022-12-07 16:45:33 -0600

    finished writing out all the commands but the get_backup function is failing

--- flows/vision-zero/database_backup/database_backup.py
+++ flows/vision-zero/database_backup/database_backup.py
@@ -1,6 +1,16 @@
 import os
-import subprocess
 from datetime import datetime
+import docker
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+
+load_dotenv()
+
+env_path = Path('.')/'postgresql.env'
+print(env_path)
+load_dotenv(dotenv_path=env_path)
+
+PGPASSWORD = os.getenv("PGPASSWORD")
 
 def get_current_datetime():
   current_datetime = datetime.now()
@@ -10,10 +20,47 @@ def get_current_datetime():
   return date, time
 
 def make_backup_directory(date):
-  path = ("./tmp/" + date + "/hello")
+  path = ("./tmp/" + date)
   os.makedirs(path, exist_ok=True)
 
-def 
+def get_backup():
+  response = (
+    docker.from_env()
+    .containers.run(
+      image="postgres:14-bullseye",
+      command="pg_dump -U vze -h db-rr.vision-zero.austinmobility.io --format=custom atd_vz_data",
+      remove=True,
+      environment=[PGPASSWORD],
+      stdin_open=True,
+      tty=True,
+      stdout=True,
+      stream=True,
+    )
+  )
+  print(response)
+  return response
 
-date, time = get_current_datetime()
-make_backup_directory(date)
+def get_aws_credentials():
+  response = (
+    docker.from_env()
+    .containers.run(
+      image="amazon/aws-cli",
+      command=["s3", "sync", "/tmp/", "s3://atd-vision-zero-database/backups/atd_vz_data_production"],
+      remove=True,
+      volumes=["`pwd`/aws_credentials:/root/.aws", "`pwd`/tmp:/tmp"],
+    )
+  )
+  return response
+
+def remove_dir():
+  os.removedirs("./tmp")
+
+def main():
+  date, time = get_current_datetime()
+  make_backup_directory(date)
+  get_backup()
+  # get_aws_credentials()
+
+
+if __name__ == "__main__":
+    main()
