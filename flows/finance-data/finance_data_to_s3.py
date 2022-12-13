c6bea8eeac86b312f893b07a8589b2753175fbd6
Charlie Henry <30810522+Charlie-Henry@users.noreply.github.com>
2022-12-13 13:43:13 -0600

    Update finance_data_to_s3.py

--- flows/finance-data/finance_data_to_s3.py
+++ flows/finance-data/finance_data_to_s3.py
@@ -14,7 +14,7 @@ import prefect
 from datetime import datetime, timedelta
 
 # Prefect
-from prefect import Flow, task
+from prefect import Flow, task, case
 from prefect.storage import GitHub
 from prefect.run_configs import UniversalRun
 from prefect.engine.state import Failed
@@ -35,8 +35,17 @@ logger = prefect.context.get("logger")
 docker_env = "test"
 docker_image = f"atddocker/atd-finance-data:{docker_env}"
 
-# Environment Variables from KV Store in Prefect
-environment_variables = get_key_value(key=f"atd_finance_data")
+
+@task(
+    name="get_env_vars",
+    max_retries=1,
+    timeout=timedelta(minutes=60),
+    retry_delay=timedelta(minutes=5),
+    # state_handlers=[handler],
+)
+def get_env_vars():
+    # Environment Variables from KV Store in Prefect
+    return get_key_value(key=f"atd_finance_data")
 
 
 @task(
@@ -50,47 +59,23 @@ def pull_docker_image():
     client = docker.from_env()
     client.images.pull("atddocker/atd-finance-data", all_tags=True)
     logger.info(docker_image)
-    return
+    return True
 
 
 @task(
     name="upload_to_s3",
-    #max_retries=1,
-    #timeout=timedelta(minutes=60),
-    #retry_delay=timedelta(minutes=5),
+    # max_retries=1,
+    # timeout=timedelta(minutes=60),
+    # retry_delay=timedelta(minutes=5),
     # state_handlers=[handler],
 )
-def upload_to_s3():
+def upload_to_s3(environment_variables, name):
     response = (
         docker.from_env()
         .containers.run(
             image=docker_image,
             working_dir=None,
-            command="python upload_to_s3.py task_orders",
-            environment=environment_variables,
-            volumes=None,
-            remove=True,
-            detach=False,
-            stdout=True,
-        )
-        .decode("utf-8")
-    )
-    logger.info(response)
-    return response
-@task(
-    name="upload_to_knack",
-    #max_retries=1,
-    #timeout=timedelta(minutes=60),
-    #retry_delay=timedelta(minutes=5),
-    # state_handlers=[handler],
-)
-def upload_to_knack():
-    response = (
-        docker.from_env()
-        .containers.run(
-            image=docker_image,
-            working_dir=None,
-            command="python s3_to_knack.py task_orders finance-purchasing",
+            command=f"python upload_to_s3.py {name}",
             environment=environment_variables,
             volumes=None,
             remove=True,
@@ -102,9 +87,36 @@ def upload_to_knack():
     logger.info(response)
     return response
 
+
+@task(
+    name="upload_to_knack",
+    # max_retries=1,
+    # timeout=timedelta(minutes=60),
+    # retry_delay=timedelta(minutes=5),
+    # state_handlers=[handler],
+)
+def upload_to_knack(environment_variables, name, app, task_orders_res):
+    response = (
+        docker.from_env()
+        .containers.run(
+            image=docker_image,
+            working_dir=None,
+            command=f"python s3_to_knack.py {name} {app}",
+            environment=environment_variables,
+            volumes=None,
+            remove=True,
+            detach=False,
+            stdout=True,
+        )
+        .decode("utf-8")
+    )
+    logger.info(response)
+    return response
+
+
 with Flow(
     # Postfix the name of the flow with the environment it belongs to
-    f"Finance Data: Upload to S3",
+    f"Finance Data Publishing",
     # Let's configure the agents to download the file from this repo
     storage=GitHub(
         repo="cityofaustin/atd-prefect",
@@ -114,9 +126,17 @@ with Flow(
     # Run config will always need the current_environment
     # plus whatever labels you need to attach to this flow
     run_config=UniversalRun(labels=["test", "atd-data02"]),
-    #schedule=Schedule(clocks=[CronClock("30 12 * * *")]),
+    schedule=Schedule(clocks=[CronClock("13 7 * * *")]),
 ) as flow:
-    flow.chain(pull_docker_image, upload_to_s3,upload_to_knack)
+    # Start: get env vars and pull the latest docker image
+    environment_variables = get_env_vars()
+    docker_res = pull_docker_image()
+
+    with case(docker_res, True):
+        task_orders_res = upload_to_s3(environment_variables, "task_orders")
+        upload_to_knack(
+            environment_variables, "task_orders", "finance-purchasing", task_orders_res
+        )
 
 if __name__ == "__main__":
     flow.run()
