4a7d75abdbeeb44138582f4578c2f086f099dd6e
Frank Hereford <frankhereford@users.noreply.github.com>
2023-01-16 12:06:26 -0600

    Labels for VZ agent

--- flows/vision-zero/prefect_bootstrap/start_agent.sh
+++ flows/vision-zero/prefect_bootstrap/start_agent.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 prefect auth login -k $VZ_PREFECT_API_KEY
-prefect agent local start -l vision-zero --import-path /root/cris_import --import-path /root/cris_import/atd-vz-data/atd-etl/app
+prefect agent local start --no-hostname-label -l vision-zero -l atd-data03 -l production --import-path /root/cris_import --import-path /root/cris_import/atd-vz-data/atd-etl/app
