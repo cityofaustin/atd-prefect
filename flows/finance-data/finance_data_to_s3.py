d9a0de2052cf74ffead66fe08276082c3184a5b4
Charlie Henry <30810522+Charlie-Henry@users.noreply.github.com>
2023-02-09 12:05:37 -0600

    cx_oracle error?

--- flows/finance-data/finance_data_to_s3.py
+++ flows/finance-data/finance_data_to_s3.py
@@ -96,7 +96,7 @@ def upload_to_knack(environment_variables, name, app, task_orders_res):
     retries=1,
     retry_delay_seconds=timedelta(minutes=5).seconds,
 )
-def upload_to_socrata(environment_variables, socrata_fl):
+def upload_to_socrata(environment_variables):
     response = (
         docker.from_env()
         .containers.run(
@@ -130,43 +130,43 @@ def main():
         )
         logger.info(task_orders_res)
 
-        finance_purchasing_res = upload_to_knack(
-            environment_variables["finance-purchasing"],
-            "task_orders",
-            "finance-purchasing",
-            task_orders_res,
-        )
-        logger.info(finance_purchasing_res)
+        # finance_purchasing_res = upload_to_knack(
+        #     environment_variables.value["finance-purchasing"],
+        #     "task_orders",
+        #     "finance-purchasing",
+        #     task_orders_res,
+        # )
+        # logger.info(finance_purchasing_res)
 
-        data_tracker_res = upload_to_knack(
-            environment_variables["data-tracker"],
-            "task_orders",
-            "data-tracker",
-            task_orders_res,
-        )
-        logger.info(data_tracker_res)
+        # data_tracker_res = upload_to_knack(
+        #     environment_variables.value["data-tracker"],
+        #     "task_orders",
+        #     "data-tracker",
+        #     task_orders_res,
+        # )
+        # logger.info(data_tracker_res)
 
-        units_res = upload_to_s3(environment_variables["data-tracker"], "units")
-        logger.info(units_res)
+        # units_res = upload_to_s3(environment_variables.value["data-tracker"], "units")
+        # logger.info(units_res)
 
-        data_tracker_res = upload_to_knack(
-            environment_variables["data-tracker"], "units", "data-tracker", units_res
-        )
-        logger.info(data_tracker_res)
+        # data_tracker_res = upload_to_knack(
+        #     environment_variables.value["data-tracker"], "units", "data-tracker", units_res
+        # )
+        # logger.info(data_tracker_res)
 
-        objects_res = upload_to_s3(environment_variables["data-tracker"], "objects")
-        logger.info(objects_res)
+        # objects_res = upload_to_s3(environment_variables.value["data-tracker"], "objects")
+        # logger.info(objects_res)
 
-        master_agreements_res = upload_to_s3(
-            environment_variables["data-tracker"], "master_agreements"
-        )
-        logger.info(master_agreements_res)
+        # master_agreements_res = upload_to_s3(
+        #     environment_variables.value["data-tracker"], "master_agreements"
+        # )
+        # logger.info(master_agreements_res)
 
-        fdus_res = upload_to_s3(environment_variables["data-tracker"], "fdus")
-        logger.info(fdus_res)
+        # fdus_res = upload_to_s3(environment_variables.value["data-tracker"], "fdus")
+        # logger.info(fdus_res)
 
-        socrata_start = if(all([task_orders_res, units_res, fdus_res]):
-            upload_to_socrata(environment_variables["data-tracker"], socrata_start)
+        # if(all([task_orders_res, units_res, fdus_res])):
+        #     upload_to_socrata(environment_variables.value["data-tracker"])
 
 
 if __name__ == "__main__":
