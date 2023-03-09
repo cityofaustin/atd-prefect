7b579072d01513e36e867b99d0432b7cce8e5f8f
Frank Hereford <frankhereford@users.noreply.github.com>
2023-03-09 13:22:03 -0600

    build DB tunnelling into flow

--- flows/vision-zero/db_permission_grantor/db_permission_grantor.py
+++ flows/vision-zero/db_permission_grantor/db_permission_grantor.py
@@ -9,6 +9,8 @@ from prefect import Flow, task
 from prefect.run_configs import UniversalRun
 import psycopg2
 
+from sshtunnel import SSHTunnelForwarder
+
 kv_store = get_key_value("Vision Zero")
 kv_dictionary = json.loads(kv_store)
 
@@ -39,11 +41,24 @@ SCHEMAS = SCHEMAS_CDL.split(", ")
 
 @task
 def grant_permissions():
+    
+    ssh_tunnel = SSHTunnelForwarder(
+        (DB_BASTION_HOST),
+        ssh_username="vz-etl",
+        ssh_private_key= '/root/.ssh/id_rsa', # will switch to ed25519 when we rebuild this for prefect 2
+        # ssh_private_key_password= creds["SSH_PKEY"],
+        remote_bind_address=(DB_RDS_HOST, 5432)
+        )
+    ssh_tunnel.start()   
+
+    print("SSH Tunnel Established (local port: " + str(ssh_tunnel.local_bind_port) + ")")
+
     pg = psycopg2.connect(
         user=DB_USER,
         password=DB_PASS,
-        host=DB_HOST,
-        port="5432",
+        host='localhost',
+        port=ssh_tunnel.local_bind_port,
+        sslmode="require",
         database=DB_NAME,)
 
     cursor = pg.cursor()
@@ -73,5 +88,5 @@ with Flow(
     logger = prefect.context.get("logger")
     result = grant_permissions()
 
-#flow.run()
-flow.register(project_name="vision-zero")
+flow.run()
+#flow.register(project_name="vision-zero")
