e4f1d3e98a3229d94c4bf600d8e0bb0696211ebd
Mike <mike.dilley@gmail.com>
2023-03-17 17:30:51 -0500

    Remove comments, update task args, and update logger

--- flows/atd-service-bot/issues_to_socrata.py
+++ flows/atd-service-bot/issues_to_socrata.py
@@ -6,8 +6,8 @@ Description: Uploads (replaces) github issue data from our atd-data-tech repo
 to an Open Data Portal dataset (AKA Socrata)
 Schedule: 21 5 * * * (UTC)
 Work queue concurrency limit: 1
-prefect deployment build flows/atd-service-bot/intake_issues.py:intake \
-    --cron "*/3 * * * *" --pool atd-data-03 -q atd-service-bot \
+prefect deployment build flows/atd-service-bot/issues_to_socrata.py:issues_to_socrata \
+    --cron "21 5 * * *" --pool atd-data-03 -q atd-service-bot \
     --name "Service Bot: Issues to Socrata" -o "deployments/atd_service_bot_socrata.yaml" \
     -sb github/atd-service-bot-staging --skip-upload \
     --description "Repo: https://github.com/cityofaustin/atd-service-bot, Uploads (replaces) github issue data from our atd-data-tech repo 
@@ -26,21 +26,20 @@ from prefect.blocks.system import Secret
 # Task to pull the latest Docker image
 @task(
     name="pull_docker_image",
-    max_retries=1,
-    timeout=timedelta(minutes=60),
-    retry_delay=timedelta(minutes=5),
-    state_handlers=[handler],
-    log_stdout=True,
+    retries=1,
+    retry_delay_seconds=5,
 )
 def pull_docker_image(docker_tag):
+    logger = get_run_logger()
+
     docker_image = f"atddocker/atd-service-bot:{docker_tag}"
     client = docker.from_env()
-    response = client.images.pull("atddocker/atd-service-bot", tag=docker_tag)
+    client.images.pull("atddocker/atd-service-bot", tag=docker_tag)
     logger.info(f"Docker Images Pulled, using: {docker_image}")
     return docker_image
 
 
-# Get the envrioment variables based on the given environment
+# Get the environment variables based on the given environment
 @task(name="get_env_vars", timeout_seconds=60)
 def get_env_vars(env):
     logger = get_run_logger()
@@ -55,14 +54,10 @@ def get_env_vars(env):
 
 
 # Issues to Socrata
-@task(
-    name="sending_issues_to_socrata",
-    max_retries=1,
-    timeout=timedelta(minutes=60),
-    retry_delay=timedelta(minutes=5),
-    log_stdout=True,
-)
+@task(name="sending_issues_to_socrata", timeout_seconds=60)
 def sending_issues_to_socrata(environment_variables, docker_image):
+    logger = get_run_logger()
+
     response = (
         docker.from_env()
         .containers.run(
@@ -81,18 +76,6 @@ def sending_issues_to_socrata(environment_variables, docker_image):
     return response
 
 
-# with Flow(
-#     # Flow Name
-#     "Service Bot: Issues to Open Data Portal",
-#     # Let's configure the agents to download the file from this repo
-#     storage=GitHub(
-#         repo="cityofaustin/atd-prefect",
-#         path="flows/service-bot/issues_to_socrata.py",
-#         ref="main",  # The branch name
-#         access_token_secret="GITHUB_ACCESS_TOKEN",
-#     ),
-#     run_config=LocalRun(labels=["atd-data02", "test"]),
-# ) as flow:
 @flow(name="Service Bot: Issues to Socrata")
 def issues_to_socrata(docker_tag="production", env="production"):
     """Uploads (replaces) github issue data from our atd-data-tech repo
