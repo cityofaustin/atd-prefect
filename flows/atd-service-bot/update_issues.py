7e69517d8ac1115f20d74805f31c6fb78bdbb29b
Mike <mike.dilley@gmail.com>
2023-03-20 17:05:43 -0500

    Fix task args

--- flows/atd-service-bot/update_issues.py
+++ flows/atd-service-bot/update_issues.py
@@ -15,56 +15,15 @@ prefect deployment build flows/atd-service-bot/update_issues.py:update \
 import docker
 
 # Prefect
-from prefect import Flow, task, Parameter
-from prefect.storage import GitHub
-from prefect.run_configs import LocalRun
-from prefect.engine.state import Failed
-from prefect.backend import get_key_value
+from prefect import flow, task, get_run_logger
 
-
-# Task to pull the latest Docker image
-@task(
-    name="pull_docker_image",
-    max_retries=1,
-    timeout=timedelta(minutes=60),
-    retry_delay=timedelta(minutes=5),
-    state_handlers=[handler],
-    log_stdout=True,
-)
-def pull_docker_image(docker_tag):
-    docker_image = f"atddocker/atd-service-bot:{docker_tag}"
-    client = docker.from_env()
-    response = client.images.pull("atddocker/atd-service-bot", tag=docker_tag)
-    logger.info(f"Docker Images Pulled, using: {docker_image}")
-    return docker_image
-
-
-# Get the envrioment variables based on the given environment
-@task(
-    name="get_env_vars",
-    task_run_name="get_env_vars",
-    max_retries=1,
-    timeout=timedelta(minutes=60),
-    retry_delay=timedelta(minutes=5),
-    state_handlers=[handler],
-    log_stdout=True,
-)
-def get_env_vars():
-    environment_variables = get_key_value(key=f"atd_service_bot_{ENV}")
-    logger.info(f"Recieved Prefect Environment Variables for: {docker_image}")
-    return environment_variables
-
+from helpers import get_env_vars, pull_docker_image
 
 # Index Issues to Knack
-@task(
-    name="update_knack_issues",
-    max_retries=1,
-    timeout=timedelta(minutes=60),
-    retry_delay=timedelta(minutes=5),
-    state_handlers=[handler],
-    log_stdout=True,
-)
+@task(name="update_knack_issues", timeout_seconds=60)
 def update_knack_issues(environment_variables, docker_image):
+    logger = get_run_logger()
+
     response = (
         docker.from_env()
         .containers.run(
@@ -83,19 +42,7 @@ def update_knack_issues(environment_variables, docker_image):
     return response
 
 
-# with Flow(
-#     # Flow Name
-#     "Service Bot: Update Issues",
-#     # Let's configure the agents to download the file from this repo
-#     storage=GitHub(
-#         repo="cityofaustin/atd-prefect",
-#         path="flows/service-bot/update_issues.py",
-#         ref="main",  # The branch name
-#         access_token_secret="GITHUB_ACCESS_TOKEN",  # Key in our Prefect secrets
-#         # Configured at: https://cloud.prefect.io/team/secrets
-#     ),
-#     run_config=LocalRun(labels=["atd-data02", "test"]),
-# ) as flow:
+@flow(name="Service Bot: Update Issues")
 def update(docker_tag="production", env="production"):
     """Updates projects in our DTS portal (Knack) with new data from Github
 
@@ -110,7 +57,7 @@ def update(docker_tag="production", env="production"):
     docker_image = pull_docker_image(docker_tag)
 
     # 3. Update our issues
-    res = update_knack_issues(environment_variables, docker_image)
+    update_knack_issues(environment_variables, docker_image)
 
 
 if __name__ == "__main__":
