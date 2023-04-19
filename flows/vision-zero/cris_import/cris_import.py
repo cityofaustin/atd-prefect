6687d46c871a661eea0af46dcaac0776e25804d4
Frank Hereford <frankhereford@users.noreply.github.com>
2023-04-19 11:21:12 -0500

    remove some unused imports

--- flows/vision-zero/cris_import/cris_import.py
+++ flows/vision-zero/cris_import/cris_import.py
@@ -10,12 +10,8 @@ import datetime
 import tempfile
 from subprocess import Popen, PIPE
 
-import requests
-
 import boto3
-import docker
 import sysrsync
-from git import Repo
 
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
@@ -24,7 +20,6 @@ import psycopg2.extras
 # Import various prefect packages and helper methods
 import prefect
 from prefect import task, Flow, Parameter
-from prefect.client import Client
 from prefect.backend import get_key_value
 from prefect.engine.state import Failed, TriggerFailed, Retrying
 from prefect.utilities.notifications import slack_notifier
@@ -43,28 +38,52 @@ from process.helpers_import import (
 kv_store = get_key_value("Vision Zero Development")
 kv_dictionary = json.loads(kv_store)
 
-SFTP_ENDPOINT = kv_dictionary["SFTP_ENDPOINT"]
-ZIP_PASSWORD = kv_dictionary["ZIP_PASSWORD"]
-VZ_ETL_LOCATION = kv_dictionary["VZ_ETL_LOCATION"]
+if False:
+    SFTP_ENDPOINT = kv_dictionary["SFTP_ENDPOINT"]
+    ZIP_PASSWORD = kv_dictionary["ZIP_PASSWORD"]
+    VZ_ETL_LOCATION = kv_dictionary["VZ_ETL_LOCATION"]
 
-RAW_AIRFLOW_CONFIG_JSON = kv_dictionary["RAW_AIRFLOW_CONFIG"]
-RAW_AIRFLOW_CONFIG = json.loads(RAW_AIRFLOW_CONFIG_JSON)
+    RAW_AIRFLOW_CONFIG_JSON = kv_dictionary["RAW_AIRFLOW_CONFIG"]
+    RAW_AIRFLOW_CONFIG = json.loads(RAW_AIRFLOW_CONFIG_JSON)
 
-AWS_DEFAULT_REGION = kv_dictionary["AWS_DEFAULT_REGION"]
-AWS_ACCESS_KEY_ID = kv_dictionary["AWS_ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY = kv_dictionary["AWS_SECRET_ACCESS_KEY"]
-AWS_CSV_ARCHIVE_BUCKET_NAME = kv_dictionary["AWS_CSV_ARCHIVE_BUCKET_NAME"]
-AWS_CSV_ARCHIVE_PATH_PRODUCTION = kv_dictionary["AWS_CSV_ARCHIVE_PATH_PRODUCTION"]
-AWS_CSV_ARCHIVE_PATH_STAGING = kv_dictionary["AWS_CSV_ARCHIVE_PATH_STAGING"]
+    AWS_DEFAULT_REGION = kv_dictionary["AWS_DEFAULT_REGION"]
+    AWS_ACCESS_KEY_ID = kv_dictionary["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY = kv_dictionary["AWS_SECRET_ACCESS_KEY"]
+    AWS_CSV_ARCHIVE_BUCKET_NAME = kv_dictionary["AWS_CSV_ARCHIVE_BUCKET_NAME"]
+    AWS_CSV_ARCHIVE_PATH_PRODUCTION = kv_dictionary["AWS_CSV_ARCHIVE_PATH_PRODUCTION"]
+    AWS_CSV_ARCHIVE_PATH_STAGING = kv_dictionary["AWS_CSV_ARCHIVE_PATH_STAGING"]
 
-DB_HOST = kv_dictionary["DB_HOST"]
-DB_USER = kv_dictionary["DB_USER"]
-DB_PASS = kv_dictionary["DB_PASS"]
-DB_NAME = kv_dictionary["DB_NAME"]
-DB_IMPORT_SCHEMA = kv_dictionary["DB_IMPORT_SCHEMA"]
+    DB_HOST = kv_dictionary["DB_HOST"]
+    DB_USER = kv_dictionary["DB_USER"]
+    DB_PASS = kv_dictionary["DB_PASS"]
+    DB_NAME = kv_dictionary["DB_NAME"]
+    DB_IMPORT_SCHEMA = kv_dictionary["DB_IMPORT_SCHEMA"]
 
-DB_BASTION_HOST = kv_dictionary["DB_BASTION_HOST"]
-DB_RDS_HOST = kv_dictionary["DB_RDS_HOST"]
+    DB_BASTION_HOST = kv_dictionary["DB_BASTION_HOST"]
+    DB_RDS_HOST = kv_dictionary["DB_RDS_HOST"]
+else:
+    SFTP_ENDPOINT = os.getenv("SFTP_ENDPOINT")
+    ZIP_PASSWORD = os.getenv("ZIP_PASSWORD")
+    VZ_ETL_LOCATION = os.getenv("VZ_ETL_LOCATION")
+
+    RAW_AIRFLOW_CONFIG_JSON = os.getenv("RAW_AIRFLOW_CONFIG")
+    RAW_AIRFLOW_CONFIG = json.loads(RAW_AIRFLOW_CONFIG_JSON)
+
+    AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_CSV_ARCHIVE_BUCKET_NAME = os.getenv("AWS_CSV_ARCHIVE_BUCKET_NAME")
+    AWS_CSV_ARCHIVE_PATH_PRODUCTION = os.getenv("AWS_CSV_ARCHIVE_PATH_PRODUCTION")
+    AWS_CSV_ARCHIVE_PATH_STAGING = os.getenv("AWS_CSV_ARCHIVE_PATH_STAGING")
+
+    DB_HOST = os.getenv("DB_HOST")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASS = os.getenv("DB_PASS")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_IMPORT_SCHEMA = os.getenv("DB_IMPORT_SCHEMA")
+
+    DB_BASTION_HOST = os.getenv("DB_BASTION_HOST")
+    DB_RDS_HOST = os.getenv("DB_RDS_HOST")
 
 # Set up slack fail handler
 handler = slack_notifier(only_states=[Failed, TriggerFailed, Retrying])
