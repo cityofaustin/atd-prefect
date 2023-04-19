e89dce204968dbefd701532db1448ed0fa2c8db2
Frank Hereford <frankhereford@users.noreply.github.com>
2023-04-19 12:01:52 -0500

    enable docker container to act as its own bastion host

--- flows/vision-zero/prefect_bootstrap/start_agent.sh
+++ flows/vision-zero/prefect_bootstrap/start_agent.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+/etc/init.d/ssh start
 prefect auth login -k $VZ_PREFECT_API_KEY
 prefect agent local start -l vision-zero --import-path /root/cris_import --import-path /root/cris_import/atd-vz-data/atd-etl/app
